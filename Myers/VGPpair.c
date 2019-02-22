d87bb0e58e8359da0a88913afcb7ecfb57bb1f8a
thegenemyers <gene.myers@gmail.com>
2019-02-22 16:28:38 +0100

    VGPpair and VGPpacbio updated to newest version of spec.     int64    gtotc, rg_totbp;

--- Myers/VGPpair.c
+++ Myers/VGPpair.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <ctype.h>
 #include <time.h>
+#include <zlib.h>
 
 #include "gene_core.h"
 
@@ -16,7 +17,7 @@ static char *Usage = "[-vsg] <forward:fastq> <reverse:fastq>";
 //  Read next line into a buffer and return a pointer to the buffer and set *plen
 //    the length of the line.  NB: replaces '\n' with '\0'.
 
-static char *read_line(FILE *input, int *plen)
+static char *read_line(gzFile input, int *plen)
 { static char *buffer;
   static int   bmax = 0;
   int len;
@@ -28,7 +29,7 @@ static char *read_line(FILE *input, int *plen)
         exit (1);
     }
 
-  if (fgets(buffer,bmax,input) == NULL)
+  if (gzgets(input,buffer,bmax) == NULL)
     return (NULL);
 
   len = strlen(buffer);
@@ -37,7 +38,7 @@ static char *read_line(FILE *input, int *plen)
       buffer = (char *) Realloc(buffer,bmax,"Reallocating read buffer");
       if (buffer == NULL)
         exit (1);
-      if (fgets(buffer+len,bmax-len,input) == NULL)
+      if (gzgets(input,buffer+len,bmax-len) == NULL)
         { fprintf(stdout,"%s: Last line of file does not end with new-line\n",Prog_Name);
           exit (1);
         }
@@ -54,24 +55,26 @@ static char *read_line(FILE *input, int *plen)
   //    of reads, # of base pairs in all reads, and maximum length of a sequence
 
 typedef struct
-  { int   nread;
-    int   maxlen;
-    int64 totbp;
-    int   ngroup;
-    int   gmaxnr;
-    int   gmaxlen;
-    int64 gmaxbp;
-    int  *gsize;
+  { int64  nread;
+    int    maxlen;
+    int64  totbp;
+    int    ngroup;
+    int64  gmaxnr;
+    int64  gmaxbp;
+    int64 *gsize;
+    int64  gmaxc;
+    int64  gtotc;
   } Header_Info;
 
-static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
-{ char  *line;
-  int    len, qlen, lineno;
-  int    nr, nread, maxlen, ngroup, gmaxnr;
-  int   *gcount, maxgroup;
-  int64  bp, totbp, gmaxbp;
-  char  *readgroup, *p;
-  int    i;
+static void Check_FastQ(gzFile input, char *file_name, Header_Info *info)
+{ char   *line;
+  int64   lineno;
+  int     len, qlen;
+  int     maxlen, ngroup, maxgroup;
+  int64   nr, nread, gmaxnr, *gcount;
+  int64   bp, totbp, gmaxbp, gtotc, gmaxc;
+  char   *readgroup, *p;
+  int     i;
 
   readgroup = NULL;
   maxgroup  = 0;
@@ -84,10 +87,15 @@ static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
   ngroup  = 0;
   gmaxnr  = 0;
   gmaxbp  = 0;
+  gtotc   = 0;
+  gmaxc   = 0;
+
+  nr = 0;
+  bp = 0;
   while ((line = read_line(input,NULL)) != NULL)
     { lineno += 1;
       if (line[0] != '@')
-        { fprintf(stdout,"%s: %s.%d: Entry header does not start with an @-sign\n",
+        { fprintf(stdout,"%s: %s.%lld: Entry header does not start with an @-sign\n",
                          Prog_Name,file_name,lineno);
           exit (1);
         }
@@ -97,8 +105,9 @@ static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
           for (i = 0; i < 4; i++)
             { p = index(p+1,':');
               if (p == NULL)
-                { fprintf(stdout,"%s: %s.%d: Entry header does not appear to be an Illumina header\n",
+                { fprintf(stdout,"%s: %s.%lld: Entry header does not appear to be",
                                  Prog_Name,file_name,lineno);
+                  fprintf(stdout," an Illumina header\n");
                   exit (1);
                 }
             }
@@ -116,10 +125,15 @@ static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
                     gmaxnr = nr; 
                   if (ngroup >= maxgroup)
                     { maxgroup = 1.2*ngroup + 10;
-                      gcount   = (int *) Realloc(gcount,sizeof(int)*maxgroup,
-                                                 "Allocating group read counts");
+                      gcount   = (int64 *) Realloc(gcount,sizeof(int64)*maxgroup,
+                                                   "Allocating group read counts");
                     }
                   gcount[ngroup++] = nr;
+
+                  len = strlen(readgroup+1);
+                  gtotc += len;
+                  if (len > gmaxc)
+                    gmaxc = len;
                 }
     
               readgroup = strdup(line);
@@ -131,7 +145,7 @@ static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
       line = read_line(input,&len);
       lineno += 1;
       if (line == NULL)
-        { fprintf(stdout,"%s: %s.%d: Incomplete fastq entry\n",Prog_Name,file_name,lineno);
+        { fprintf(stdout,"%s: %s.%lld: Incomplete fastq entry\n",Prog_Name,file_name,lineno);
           exit (1);
         }
 
@@ -143,22 +157,22 @@ static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
       line = read_line(input,NULL);
       lineno += 1;
       if (line == NULL)
-        { fprintf(stdout,"%s: %s.%d Incomplete fastq entry\n",Prog_Name,file_name,lineno);
+        { fprintf(stdout,"%s: %s.%lld Incomplete fastq entry\n",Prog_Name,file_name,lineno);
           exit (1);
         }
       if (line[0] != '+')
-        { fprintf(stdout,"%s: %s.%d Divider line does not start with a +-sign\n",
+        { fprintf(stdout,"%s: %s.%lld Divider line does not start with a +-sign\n",
                          Prog_Name,file_name,lineno);
           exit (1);
         }
       line = read_line(input,&qlen);
       lineno += 1;
       if (line == NULL)
-        { fprintf(stdout,"%s: %s.%d Incomplete fastq entry\n",Prog_Name,file_name,lineno);
+        { fprintf(stdout,"%s: %s.%lld Incomplete fastq entry\n",Prog_Name,file_name,lineno);
           exit (1);
         }
       if (len != qlen)
-        { fprintf(stdout,"%s: %s.%d: QV line does not have the same length as sequence line\n",
+        { fprintf(stdout,"%s: %s.%lld: QV line does not have the same length as sequence line\n",
                          Prog_Name,file_name,lineno);
           exit (1);
         }
@@ -173,10 +187,15 @@ static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
         gmaxnr = nr; 
       if (ngroup >= maxgroup)
         { maxgroup = 1.2*ngroup + 10;
-          gcount   = (int *) Realloc(gcount,sizeof(int)*maxgroup,
-                                     "Allocating group read counts");
+          gcount   = (int64 *) Realloc(gcount,sizeof(int64)*maxgroup,
+                                       "Allocating group read counts");
         }
       gcount[ngroup++] = nr;
+
+      len = strlen(readgroup+1);
+      gtotc += len;
+      if (len > gmaxc)
+        gmaxc = len;
     }
 
   info->nread  = nread;
@@ -186,13 +205,15 @@ static void Check_FastQ(FILE *input, char *file_name, Header_Info *info)
   info->gmaxnr = gmaxnr;
   info->gmaxbp = gmaxbp;
   info->gsize  = gcount;
+  info->gtotc  = gtotc;
+  info->gmaxc  = gmaxc;
 
   free(readgroup);
 }
 
   //  In a second pass, output .irp format for each pair
 
-static void Output_Pairs(FILE *forward, FILE *reverse, int *gcount)
+static void Output_Pairs(gzFile forward, gzFile reverse, int64 *gcount)
 { char *line;
   int   len;
   int   DO_QV;
@@ -213,7 +234,7 @@ static void Output_Pairs(FILE *forward, FILE *reverse, int *gcount)
           if (readgroup == NULL || strcmp(readgroup,line) != 0)
             { free(readgroup);
               readgroup = strdup(line);
-              printf("g %d %ld %s\n",gcount[gc++],strlen(readgroup+1)+1,readgroup+1);
+              printf("g %lld %ld %s\n",gcount[gc++],strlen(readgroup+1)+1,readgroup+1);
             } 
         }
 
@@ -236,9 +257,35 @@ static void Output_Pairs(FILE *forward, FILE *reverse, int *gcount)
 }
 
 int main(int argc, char *argv[])
-{ FILE       *input1, *input2;
+{ gzFile      input1, input2;
   char       *fname1, *fname2;
   Header_Info stats1, stats2;
+
+  //  Output file type and provenance
+
+  { int    i, len;
+    char   date[26];
+    time_t seconds;
+
+    printf("1 3 seq 1 0\n");
+    printf("2 3 irp\n");
+    printf("# ! 1\n");
+    len = -1;
+    for (i = 1; i < argc; i++)
+      len += strlen(argv[i]);
+    printf("+ ! %d\n",len+34);
+    if (len > 24)
+      printf("@ ! %d\n",len);
+    else
+      printf("@ ! 24\n");
+    printf("! 7 VGPpair 3 1.0 %d",len);
+    for (i = 1; i < argc; i++)
+      printf(" %s",argv[i]);
+    seconds = time(NULL);
+    ctime_r(&seconds,date);
+    date[24] = '\0';
+    printf(" 24 %s\n",date);
+  }
 
   //  Parse command line options
 
@@ -276,20 +323,28 @@ int main(int argc, char *argv[])
 
   { char *pwd1, *pwd2;
 
-    fname1 = Root(argv[1],".fastq");
+    fname1 = Root(argv[1],".fastq.gz");
     pwd1   = PathTo(argv[1]);
-    input1 = fopen(Catenate(pwd1,"/",fname1,".fastq"),"r");
+    input1 = gzopen(Catenate(pwd1,"/",fname1,".fastq"),"r");
     if (input1 == NULL)
-      { fprintf(stdout,"%s: Cannot open %s either as a .fastq or .fasta\n",Prog_Name,argv[1]);
-        exit (1);
+      { fname1 = Root(argv[1],".fastq");
+        input1 = gzopen(Catenate(pwd1,"/",fname1,".fastq"),"r");
+        if (input1 == NULL)
+          { fprintf(stdout,"%s: Cannot open %s either as a .fastq\n",Prog_Name,argv[1]);
+            exit (1);
+          }
       }
 
-    fname2 = Root(argv[2],".fastq");
+    fname2 = Root(argv[2],".fastq.gz");
     pwd2   = PathTo(argv[2]);
-    input2 = fopen(Catenate(pwd2,"/",fname2,".fastq"),"r");
+    input2 = gzopen(Catenate(pwd2,"/",fname2,".fastq"),"r");
     if (input2 == NULL)
-      { fprintf(stdout,"%s: Cannot open %s either as a .fastq or .fasta\n",Prog_Name,argv[2]);
-        exit (1);
+      { fname2 = Root(argv[2],".fastq");
+        input2 = gzopen(Catenate(pwd2,"/",fname2,".fastq"),"r");
+        if (input2 == NULL)
+          { fprintf(stdout,"%s: Cannot open %s either as a .fastq\n",Prog_Name,argv[2]);
+            exit (1);
+          }
       }
 
     if (strcmp(fname1,fname2) == 0)
@@ -327,44 +382,15 @@ int main(int argc, char *argv[])
   Check_FastQ(input2,fname2,&stats2);
 
   if (stats1.nread != stats2.nread)
-    { fprintf(stdout,"%s: Cannot open %s either as a .fastq or .fasta\n",Prog_Name,argv[2]);
+    { fprintf(stdout,"%s: Number of reads in two files not equal: %lld vs %lld\n",
+                     Prog_Name,stats1.nread,stats2.nread);
       exit (1);
     }
 
-  //  Output file type and provenance
+  //  Output .irp sizes
 
-  { int    len;
-    char   date[26];
-    time_t seconds;
-
-    printf("1 3 irp 1 0\n");
-    printf("# ! 1\n");
-    printf("! 7 VGPpair 1 0");
-    if (VERBOSE+SEQ_ONLY+GROUP)
-      { len = strlen(argv[1]) + strlen(argv[2]) + VERBOSE + SEQ_ONLY + GROUP + 3;
-        printf(" %d -",len);
-        if (VERBOSE)
-          printf("v");
-        if (SEQ_ONLY)
-          printf("s");
-        if (GROUP)
-          printf("g");
-      }
-    else
-      { len = strlen(argv[1]) + strlen(argv[2]) + 1;
-        printf(" %d",len);
-      }
-    printf(" %s %s",argv[1],argv[2]);
-    seconds = time(NULL);
-    ctime_r(&seconds,date);
-    date[24] = '\0';
-    printf(" 24 %s\n",date);
-  }
-
-  //  Output .irp header
-
-  printf("# P %d\n",stats1.nread);
-  printf("# S %d\n",2*stats1.nread);
+  printf("# P %lld\n",stats1.nread);
+  printf("# S %lld\n",2*stats1.nread);
   printf("+ S %lld\n",stats1.totbp + stats2.totbp);
   if (stats1.maxlen > stats2.maxlen)
     printf("@ S %d\n",stats1.maxlen);
@@ -372,7 +398,7 @@ int main(int argc, char *argv[])
     printf("@ S %d\n",stats2.maxlen);
 
   if ( ! SEQ_ONLY)
-    { printf("# Q %d\n",2*stats1.nread);
+    { printf("# Q %lld\n",2*stats1.nread);
       printf("+ Q %lld\n",stats1.totbp + stats2.totbp);
       if (stats1.maxlen > stats2.maxlen)
         printf("@ Q %d\n",stats1.maxlen);
@@ -382,11 +408,13 @@ int main(int argc, char *argv[])
 
   if (GROUP)
     { printf("# g %d\n",stats1.ngroup);
-      printf("%% g # P %d\n",stats1.gmaxnr);
-      printf("%% g # S %d\n",2*stats1.gmaxnr);
+      printf("+ g %lld\n",stats1.gtotc);
+      printf("@ g %lld\n",stats1.gmaxc);
+      printf("%% g # P %lld\n",stats1.gmaxnr);
+      printf("%% g # S %lld\n",2*stats1.gmaxnr);
       printf("%% g + S %lld\n",stats1.gmaxbp + stats2.gmaxbp);
       if ( ! SEQ_ONLY)
-        { printf("%% g # Q %d\n",2*stats1.gmaxnr);
+        { printf("%% g # Q %lld\n",2*stats1.gmaxnr);
           printf("%% g + Q %lld\n",stats1.gmaxbp + stats2.gmaxbp);
         }
     }
@@ -398,14 +426,14 @@ int main(int argc, char *argv[])
       fflush(stderr);
     }
 
-  rewind(input1);
-  rewind(input2);
+  gzrewind(input1);
+  gzrewind(input2);
   Output_Pairs(input1,input2,stats1.gsize);
 
   //  Tidy up just for good form
 
-  fclose(input2);
-  fclose(input1);
+  gzclose(input2);
+  gzclose(input1);
   free(fname2);
   free(fname1);
   exit (0);
